f528d02c80c4c8473bfd9b860da395c3dfd22059
AleksandrRazhev <alexandrrazh@gmail.com>
2022-02-06 13:54:58 +0300

    added controlled comonent in form

--- src/components/EmployeesAddForm/EmployeesAddForm.js
+++ src/components/EmployeesAddForm/EmployeesAddForm.js
@@ -1,23 +1,51 @@
 import './EmployeesAddForm.css';
+import { Component } from 'react/cjs/react.production.min';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Добавьте нового сотрудника</h3>
-      <form
-        className="add-form d-flex">
-        <input type="text"
-          className="form-control new-post-label"
-          placeholder="Как его зовут?" />
-        <input type="number"
-          className="form-control new-post-label"
-          placeholder="З/П в $?" />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: '',
+    }
+  }
 
-        <button type="submit"
-          className="btn btn-outline-light">Добавить</button>
-      </form>
-    </div>
-  )
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  render() {
+
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form
+          className="add-form d-flex">
+          <input type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            value={name}
+            name='name'
+            onChange={this.onValueChange}
+          />
+          <input type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            value={salary}
+            name='salary'
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit"
+            className="btn btn-outline-light">Добавить</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm;
